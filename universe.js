a5f8309a6cecb188a11304ae8e7f7b24becf2506
arasad1 <asaduzzamanaj07@gmail.com>
2023-03-02 23:13:22 +0600

    show default 6 cards

--- universe.js
+++ universe.js
@@ -2,10 +2,32 @@
 const loadCardData = () => {
     fetch('https://openapi.programming-hero.com/api/ai/tools')
         .then(res => res.json())
-        .then(data => showCardData(data.data.tools))
+        .then(data => showDataInCard(data.data.tools))
 }
-const showCardData = (items) => {
-    console.log(items)
+const showDataInCard = (items) => {
+    const mainContainer = document.getElementById('main-container');
+    // show default 6 cards in display
+    items = items.slice(0, 6)
+    for (const item of items) {
+        console.log(item)
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add('col')
+        cardDiv.innerHTML = `
+        <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+    </div>
+        `
+        mainContainer.appendChild(cardDiv);
+    }
+
 }
 
 loadCardData();
