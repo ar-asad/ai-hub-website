3901152d66643a367ce030f7435f87a2f9327eeb
arasad1 <asaduzzamanaj07@gmail.com>
2023-03-03 14:32:49 +0600

    set all data in modal

--- universe.js
+++ universe.js
@@ -51,47 +51,48 @@ const showItemsDetails = (itemsDetails) => {
     <div class="col">
     <div class="card h-100">
         <div class="card-body details-card-first rounded">
-            <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, fuga.
+            <h5>${itemsDetails.description}
             </h5>
             <div class="d-flex align-items-center justify-content-between m-2 ">
-                <div style="color: #03A30A;" class="bg-light p-4 rounded">amrs</div>
-                <div style="color: #F28927;" class="bg-light p-4 rounded">tomar</div>
-                <div style="color: #EB5757;" class="bg-light p-4 rounded">somar</div>
-            </div>
-            <div class="d-flex align-items-center justify-content-between ">
-                <div>
-                    <h4>fetuares</h4>
-                    <ul>
-                        <li>card</li>
-                        <li>card</li>
-                        <li>card</li>
-                    </ul>
+                <div style="color: #03A30A;" class="bg-light p-4 rounded">
+                <p>${itemsDetails.pricing[0].price ? itemsDetails.pricing[0].price : "Free of coast/"} <br>${itemsDetails.pricing[0].plan}</p>
+                </div >
+                <div style="color: #F28927;" class="bg-light p-4 rounded"> <p>${itemsDetails.pricing[1].price ? itemsDetails.pricing[1].price : "Free of coast/"} <br>${itemsDetails.pricing[1].plan}</p>
                 </div>
-                <div>
-                    <h4>fetuares</h4>
-                    <ul>
-                        <li>card</li>
-                        <li>card</li>
-                        <li>card</li>
-                    </ul>
+                <div style="color: #EB5757;" class="bg-light p-4 rounded"> <p>${itemsDetails.pricing[2].price ? itemsDetails.pricing[2].price : "Free of coast/"} <br>${itemsDetails.pricing[2].plan}</p>
                 </div>
-            </div>
+            </div >
+    <div class="d-flex align-items-center justify-content-between ">
+        <div>
+            <h4>Features</h4>
+            <ul>
+                <li>${itemsDetails.features[1].feature_name}</li>
+                <li>${itemsDetails.features[2].feature_name}</li>
+                <li>${itemsDetails.features[3].feature_name}</li>
+            </ul>
+        </div>
+        <div>
+            <h4>Integrations</h4>
+            <ul>
+                <li>${itemsDetails.integrations[0]}</li>
+                <li>${itemsDetails.integrations[1]}</li>
+                <li>${itemsDetails.integrations[2]}</li>
+            </ul>
         </div>
     </div>
-</div>
-<div class="col">
-    <div class="card h-100">
-        <img class="p-3" src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title text-center">Card title</h5>
-            <p class="card-text text-center">This is a longer card with supporting text
-                below as a
-                natural lead-in to additional content. This content is a little bit
-                longer.</p>
+        </div >
+    </div >
+</div >
+    <div class="col">
+        <div class="card h-100">
+            <img class="p-3 rounded-5" src=${itemsDetails.image_link[0]} alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-center">${itemsDetails.input_output_examples[0].input}</h5>
+                    <p>${itemsDetails.input_output_examples[1].output ? itemsDetails.input_output_examples[1].output : "No! Not Yet! Take a break!!!"}</p>
+                </div>
         </div>
     </div>
-</div>
-    `
+`
 
 }
 
